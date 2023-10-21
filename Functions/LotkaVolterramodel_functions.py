9ac82b66b5528443ddace18e7a2a66b45a1b8a4e
noemisgambelluri <noemi.ginnyfangirl@gmail.com>
2023-10-21 22:33:13 +0100

    create function that solves Lotka Volterra

--- Functions/LotkaVolterramodel_functions.py
+++ Functions/LotkaVolterramodel_functions.py
@@ -1,11 +1,16 @@
 #=======================================================================
 # Author: Noemi Sgambelluri
-# Date: 18 October, 2023
+# Date: 15 October, 2023
 #
 # Lotka-Volterra model (also known as 'Predator-Prey equations)
 #
 # Aim: To define the functions needed to simulate the Lotka-Volterra model.
 #=======================================================================
+
+import numpy as np
+from scipy.integrate import find_peaks
+from scipy.integrate import odeint
+from scipy.signal import find_peaks
 
 def LotkaVolterra(variables, parameters):
 
@@ -13,12 +18,15 @@ def LotkaVolterra(variables, parameters):
 
     This function defines the Lotka-Volterra equations for a two species system.
 
-    Parameters
-        variables : current population levels of the two interacting species
-        parameters : parameters that define the interaction between the species
+    Parameters:
+        variables : array
+            A list containing current population levels of the two interacting species.
+
+        parameters : float
+            Lotka-Volterra model parameters that define the interaction between the species.
 
     Returns:
-        Rates of change of both the prey and the predator populations (dxdt, dydt)
+        Rates of change of both the prey and the predator populations (dxdt, dydt).
 
     """
 
@@ -31,26 +39,43 @@ def LotkaVolterra(variables, parameters):
 
     return dxdt, dydt
 
-def Equilibria(parameters):
-        
+def SolveLotkaVolterra(parameters, initial_conditions, t_max, num_points):
+   
     """
-    This function computes the two equilibrium points, i.e. the population values at which both
-    the predator and prey populations remain constant over time. 
-    
 
-    Parameters
-        parameters : parameters that define the interaction between the species
+    Numerically solve the Lotka-Volterra equations.
+
+    Parameters:
+    parameters : float
+        Lotka-Volterra model parameters.
+
+    initial_conditions : float
+        Initial populations of prey and predator.
+
+    t_max : float
+        Maximum time for simulation.
+
+    num_points : int
+        Number of time points for simulation.
 
     Returns:
-        equilibrium points values
+    solution : array
+        An array containing the prey and predator populations at different time points.
+    t : array
+        An array containing the time points.
+
     """
+    #create a time vector
+    t = np.linspace(0, t_max, num_points)
+    #set initial conditions
+    x0, y0 = initial_conditions
     alpha, beta, delta, gamma = parameters
+    #numerically solve the Lotka-Volterra equations
+    sol = odeint(LotkaVolterra, initial_conditions, t, args= (alpha, beta, delta, gamma))
 
-    #mathematical definition of the equilibrium points of the Lotka-Volterra equations
-    eq_point1 = (0.0, 0.0)
-    eq_point2 = (gamma/delta, alpha/beta)
+    return sol, t 
 
-    return eq_point1, eq_point2
+
 
 
 
