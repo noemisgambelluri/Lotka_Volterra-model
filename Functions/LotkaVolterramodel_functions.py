9602483e6261a422cf237c094f6850626b1da29e
noemisgambelluri <noemi.ginnyfangirl@gmail.com>
2023-10-21 14:57:54 +0100

    LotkaVolterra function description

--- Functions/LotkaVolterramodel_functions.py
+++ Functions/LotkaVolterramodel_functions.py
@@ -1,19 +1,35 @@
+#=======================================================================
+# Author: Noemi Sgambelluri
+# Date: 18 October, 2023
+#
+# Lotka-Volterra model (also known as 'Predator-Prey equations)
+#
+# Aim: To define the functions needed to simulate the Lotka-Volterra model.
+#=======================================================================
 
 
 def LotkaVolterra(variables, parameters):
-    # Define the Lotka-Volterra equations for a two specoes system
-    # 'variables': array of the current population levels of the two interacting species
-    # 't': represents time
-    # 'parameters': array of parameters that define the interaction between the species
+
+    """
+
+    This function defines the Lotka-Volterra equations for a two species system.
+
+    Parameters
+        variables : current population levels of the two interacting species
+        parameters : parameters that define the interaction between the species
+
+    Returns:
+        Rates of change of both the prey and the predator populations (dxdt, dydt)
+
+    """
 
     x, y = variables
     alpha, beta, delta, gamma = parameters
 
-    #Lotka-Volterra (also known as 'Predator-Prey) equations mathematical implementation:
+    #Mathematical implementation of the Lotka-Volterra equations:
     dxdt = alpha * x - beta * x * y
     dydt = delta * x * y - gamma * y
 
-    #return the rates of change of both the prey and predator populations
     return dxdt, dydt
 
 
