440b67d63d91ca38f3afcf05102e1da00a506ef2
noemisgambelluri <noemi.ginnyfangirl@gmail.com>
2023-10-29 18:11:20 +0000

    modification test functions comments

--- LVTestFunctions.py
+++ LVTestFunctions.py
@@ -2,7 +2,7 @@
 # Author: Noemi Sgambelluri
 # Date: 28 October, 2023
 #
-# Tests
+# Tests of model functions
 #
 # Aim: To test functions in LotkaVolterraModel.py.
 ########################################################################
@@ -17,12 +17,16 @@ def test_LotkaVolterra_computation():
 
     """
     Procedure:
-    1. Initialize random seed
-    2. Generate initial configuration given a certain number of particles (num_part) and linear dimension of space (space_dim)
+    1. Initialize parameters values
+    2. Initialize variables (preys, predators) values
+    3. Initialize time value
+    3. Compute dxdt and dydt values
     ---------
     Verification:
-    3. The lenght of the output is 3
-    4. The lenght of the output components (coordinates x, y and orietations of the particles) is num_part
+    3. The computation of dxdt performed by the function has to correspond to the expected
+       computed dxdt value
+    4. The computation of dydt performed by the function has to correspond to the expected
+       computed dydt value
     """
 
     alpha = 1.2
@@ -42,11 +46,19 @@ def test_SolveLotkaVolterra_length(t_max, num_points):
     """
     Procedure:
     1. Initialize random seed
-    2. Generate initial configuration given a certain number of particles (num_part) and linear dimension of space (space_dim)
+    2. Configurate time interval for Lotka Volterra model given a maximum time  
+    (t_max) and number of time points (num_points)
+    3. Initialize preys and predators initial conditions and paramaters values
+    4. Solve Lotka Volterra equations for each time point
+
     ---------
     Verification:
-    3. The lenght of the output is 3
-   4. The lenght of the output components (coordinates x, y and orietations of the particles) is num_part
+    3. The lenght of the solution array has to be the equal to the number of time points
+    4. The solution has to be a 2D array 
+    5. The lenght of the time array returned by the function has to be equal to the number 
+       of time points
+    6. All time points in the time array returned by the function has to be between 0 and 
+       the max set time value
     """
 
     np.random.seed(5)
@@ -63,6 +75,24 @@ def test_SolveLotkaVolterra_length(t_max, num_points):
 
 @given(alpha = st.floats(0, 2.0), beta = st.floats(0.1, 2.0), delta = st.floats(0.1, 2.0), gamma = st.floats(0, 2.0))
 def test_Equilibria_length(alpha, beta, delta, gamma):
+
+    """
+    Procedure:
+    1. Initialize random seed
+    2. Configurate time interval for Lotka Volterra model given a maximum time  
+    (t_max) and number of time points (num_points)
+    3. Initialize preys and predators initial conditions and paramaters values
+    4. Solve Lotka Volterra equations for each time point
+
+    ---------
+    Verification:
+    3. The lenght of the solution array has to be the equal to the number of time points
+    4. The solution has to be a 2D array 
+    5. The lenght of the time array returned by the function has to be equal to the number 
+       of time points
+    6. All time points in the time array returned by the function has to be between 0 and 
+       the max set time value
+    """
 
     np.random.seed(5)
 
