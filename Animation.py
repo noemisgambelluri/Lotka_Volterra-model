13bdedd57cd3617c18f322de06846bdc4b8adce1
noemisgambelluri <noemi.ginnyfangirl@gmail.com>
2023-11-02 00:14:58 +0000

    add comments to speed of animation

--- Animation.py
+++ Animation.py
@@ -49,7 +49,7 @@ line1, = ax.plot(time, pred_rates, label = 'Preys', lw = 2)
 line2, = ax.plot(time, prey_rates, label = 'Predators', linestyle = '--', lw = 2)
 
 # Create animation
-animation_speed = 3
+animation_speed = 3     #speed up animation x3 (change according to your taste)
 def animate(i):
 
     i = i * animation_speed
